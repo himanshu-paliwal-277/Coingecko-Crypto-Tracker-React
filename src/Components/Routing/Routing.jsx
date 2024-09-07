17594c1a3475e02c847ea49839cf59a6cd80970f
Himanshu Paliwal <hpaliwal364@gmail.com>
2024-09-07 09:49:20 +0530

    added error boundary

--- src/Components/Routing/Routing.jsx
+++ src/Components/Routing/Routing.jsx
@@ -2,35 +2,38 @@ import { Route, Routes } from "react-router-dom";
 import MainLayout from "../../Pages/Layout";
 import { lazy, Suspense } from "react";
 import PageLoader from "../PageLoader/PageLoader";
+import CustomErrorBoundary from "../CustomErrorBoundary/CustomErrorBoundary";
 
-const Home = lazy(() => import('../../pages/Home'));
-const CoinDetailsPage = lazy(() => import('../../pages/CoinDetailsPage'));
-
+const Home = lazy(() => import("../../pages/Home"));
+const CoinDetailsPage = lazy(() => import("../../pages/CoinDetailsPage"));
 
 function Routing() {
-    return (
-        <>
-            <Routes>
-                <Route path="/" element={<MainLayout />}>
-                    {/* <Route index element={<Home />} />
-                    <Route path="/details/:coinId" element={<CoinDetailsPage />} /> */}
-
-                    <Route index element={
-                    <Suspense fallback={<PageLoader />}>
-                        <Home />
-                    </Suspense>
-                } />
-                <Route path="/details/:coinId" element={
-
-                    <Suspense fallback={<PageLoader />}>
-                        <CoinDetailsPage />
-                    </Suspense>
-
-                } />
-                </Route>
-            </Routes>
-        </>
-    )
+  return (
+    <>
+      <CustomErrorBoundary>
+        <Routes>
+          <Route path="/" element={<MainLayout />}>
+            <Route
+              index
+              element={
+                <Suspense fallback={<PageLoader />}>
+                  <Home />
+                </Suspense>
+              }
+            />
+            <Route
+              path="/details/:coinId"
+              element={
+                <Suspense fallback={<PageLoader />}>
+                  <CoinDetailsPage />
+                </Suspense>
+              }
+            />
+          </Route>
+        </Routes>
+      </CustomErrorBoundary>
+    </>
+  );
 }
 
 export default Routing;
